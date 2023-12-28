f40edd424b13c76b490e19a22924dedb7357cc03
Lara <laragalvanim@gmail.com>
2023-12-28 20:10:45 -0300

    Start creating some unit tests for the domain layer

---
+++ tests/ApartmentFinder.Domain.UnitTests/Bookings/CancelBookingTests.cs
@@ -0,0 +1,77 @@
+﻿using FluentAssertions;
+using ApartmentFinder.Domain.Users;
+using ApartmentFinder.Domain.Shared;
+using ApartmentFinder.Domain.Bookings;
+using ApartmentFinder.Domain.Apartments;
+using ApartmentFinder.Domain.Bookings.Events;
+
+namespace ApartmentFinder.Domain.UnitTests.Bookings;
+
+public class CancelBookingTests : BaseTest
+{
+	private Apartment CreateApartment()
+	{
+		return new Apartment(
+			ApartmentId.New(),
+			new Name("Apartment 1"),
+			new Description("Description here"),
+			new Address("England", "London", "EC1A 1AE", "London", "King Edward St"),
+			new Money(1000, Currency.Eur),
+			new Money(50, Currency.Eur),
+			new List<Amenity>());
+	}
+
+	private Booking BookApartment(DateTime startDate, DateTime endDate)
+	{
+		var apartment = CreateApartment();
+		return Booking.Reserve(
+			apartment,
+			UserId.New(),
+			DateRange.Create(DateOnly.FromDateTime(startDate), DateOnly.FromDateTime(endDate)),
+			DateTime.UtcNow,
+			new PricingService());
+	}
+
+	[Fact]
+	public void Cancel_Should_Raise_BookingCancelledDomainEvent()
+	{
+		// Arrange
+		var booking = BookApartment(new DateTime(2024, 10, 12), new DateTime(2024, 10, 18));
+		booking.Confirm(DateTime.UtcNow);
+
+		// Act
+		booking.Cancel(DateTime.UtcNow);
+
+		// Assert
+		var bookingCancelledDomainEvent = AssertDomainEventWasPublished<BookingCancelledDomainEvent>(booking);
+
+		bookingCancelledDomainEvent.BookingId.Should().Be(booking.Id);
+	}
+
+	[Fact]
+	public void Cancel_Before_Confirmation_Should_Not_Be_Allowed()
+	{
+		// Arrange
+		var booking = BookApartment(new DateTime(2024, 10, 12), new DateTime(2024, 10, 18));
+
+		// Act
+		var result = booking.Cancel(DateTime.UtcNow);
+
+		// Assert
+		result.IsFailure.Should().BeTrue();
+	}
+
+	[Fact]
+	public void Cancel_After_Start_Date_Should_Not_Be_Allowed()
+	{
+		// Arrange
+		var booking = BookApartment(new DateTime(2023, 05, 10), new DateTime(2024, 06, 10));
+
+		// Act
+		var result = booking.Cancel(DateTime.UtcNow);
+
+		// Assert
+		result.IsFailure.Should().BeTrue();
+	}
+}
+
---
+++ tests/ApartmentFinder.Domain.UnitTests/Users/RegisterUserTests.cs
@@ -0,0 +1,26 @@
+﻿using FluentAssertions;
+using ApartmentFinder.Domain.Users;
+using ApartmentFinder.Domain.Users.Events;
+
+namespace ApartmentFinder.Domain.UnitTests.Users;
+
+public class RegisterUserTests : BaseTest
+{
+	[Fact]
+	public void Create_Should_Raise_UserCreatedDomainEvent()
+	{
+		// Arrange
+		var firstName = new FirstName("first");
+		var lastName = new LastName("last");
+		var email = new Email("test@test.com");
+
+		// Act
+		var user = User.Create(firstName, lastName, email);
+
+		// Assert
+		var userCreatedDomainEvent = AssertDomainEventWasPublished<UserCreatedDomainEvent>(user);
+
+		userCreatedDomainEvent.UserId.Should().Be(user.Id);
+	}
+}
+
