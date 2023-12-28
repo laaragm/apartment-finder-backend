9914fc51066a5f35ead18f0e3788ed43c531e6df
Lara <laragalvanim@gmail.com>
2023-12-28 20:34:44 -0300

    Add some architecture tests

---
+++ tests/ApartmentFinder.ArchitectureTests/BaseTest.cs
@@ -0,0 +1,14 @@
+﻿using System.Reflection;
+using ApartmentFinder.Infrastructure;
+using ApartmentFinder.Domain.Abstractions;
+using ApartmentFinder.Application.Abstractions.Messaging;
+
+namespace ApartmentFinder.ArchitectureTests;
+
+
+public class BaseTest
+{
+	protected static Assembly ApplicationAssembly => typeof(IBaseCommand).Assembly;
+	protected static Assembly DomainAssembly => typeof(IEntity).Assembly;
+	protected static Assembly InfrastructureAssembly => typeof(ApplicationDbContext).Assembly;
+}
---
+++ tests/ApartmentFinder.ArchitectureTests/Domain/DomainTests.cs
@@ -0,0 +1,20 @@
+﻿using FluentAssertions;
+using NetArchTest.Rules;
+using ApartmentFinder.Domain.Abstractions;
+
+namespace ApartmentFinder.ArchitectureTests.Domain;
+
+public class DomainTests : BaseTest
+{
+	[Fact]
+	public void Domain_Event_Should_Have_DomainEvent_Postfix()
+	{
+		var result = Types.InAssembly(DomainAssembly)
+			.That()
+			.ImplementInterface(typeof(IDomainEvent))
+			.Should().HaveNameEndingWith("DomainEvent")
+			.GetResult();
+
+		result.IsSuccessful.Should().BeTrue();
+	}
+}
